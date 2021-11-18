deed600eaf53107e85a171a8bbbe42be7af46c65
jaisonrobson <xd_jrg@hotmail.com>
2021-11-18 09:09:04 -0300

    feito exemplo basico de contador com uso total de reducer para incrementar e decrementar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React, { useState, useReducer } from 'react'
+import React, { useReducer } from 'react'
 
 function reducer(state, action) {
-    return { count: state.count + 1 }
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 }
+        case 'decrement':
+            return { count: state.count - 1 }
+        default:
+            return state
+    }
 }
 
 const App = () => {
     const [state, dispatch] = useReducer(reducer, { count: 0 })
-    const [count, setCount] = useState(0)
 
     function increment() {
-        // setCount(prevCount => prevCount + 1)
-        dispatch()
+        dispatch({ type: 'increment' })
     }
 
     function decrement() {
-        setCount(prevCount => prevCount - 1)
+        dispatch({ type: 'decrement' })
     }
 
     return (
